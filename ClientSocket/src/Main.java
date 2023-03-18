373a1cf0cac25c1a69b6bcd27b900ef577a9a273
Joao1512 <jvictorsimao@hotmail.com>
2023-03-18 20:58:17 -0300

    client application

---
+++ ClientSocket/src/Main.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    private static Client client;
+    public static void main(String[] args) throws IOException {
+        client = new Client();
+        client.startConnection("127.0.0.1", 8080);
+
+        String message = "";
+        while(!message.equals(".")) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Você: ");
+            message = scanner.nextLine();
+
+            String res = client.sendMessage(message);
+            System.out.println("Server: " + res);
+        }
+
+        client.stopConnection();
+    }
+}
